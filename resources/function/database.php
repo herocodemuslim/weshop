f5204c92237060c4dca974f1e6688a45c78ec371
Fadli Hidayatullah <fadli.hidayatullah.hero@gmail.com>
2018-05-25 09:46:20 +0700

    Pembuatan file koneksi database

---
+++ resources/function/database.php
@@ -0,0 +1,10 @@
+<?php 
+
+$host = 'localhost';
+$user = 'root';
+$password = 'ngopi';
+$database = 'weshop';
+
+$link = mysqli_connect($host, $user, $password, $database) or exit('Koneksi ke database gagal.');
+
+?>
